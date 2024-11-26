7931fa5d3227e10f86c749a69d7c7a0712cd3070
BrianSeong99 <brianseong.dev@gmail.com>
2024-11-26 23:11:37 +0900

    fix: update lxly.js version in package.json

--- examples/lxly/erc20/claim_asset.js
+++ examples/lxly/erc20/claim_asset.js
@@ -1,13 +1,13 @@
 const { getLxLyClient, tokens, configuration, from } = require('../../utils_lxly');
 
 const execute = async () => {
-    const bridgeTransactionHash = "0x89e43e85eae56d42ae09a23f1c89c47e9815b7f88d05d4a9427ee13b4772e652";
+    const bridgeTransactionHash = "0xe80c84456d6e77dec4fb07e21242e714c42fabf382b6c538258cf2c7dc312f60";
     const client = await getLxLyClient('testnet');
 
     const token = client.erc20(tokens[0].ether, 0);
     const sourceNetworkId = 1;
 
-    const result = await token.claimAsset(bridgeTransactionHash, sourceNetworkId, {returnTransaction: true});
+    const result = await token.claimAsset(bridgeTransactionHash, sourceNetworkId, {returnTransaction: false});
     console.log("result", result);
     const txHash = await result.getTransactionHash();
     console.log("txHash", txHash);
